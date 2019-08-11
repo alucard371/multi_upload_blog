d9ff286d54904838ee98472016984a10e9d6d007
alucard371 <test8q@gmail.com>
2019-08-11 08:14:48 +0200

    run new migrations for models post and  post_image

--- multi_upload_blog/database/migrations/2019_08_11_061159_create_post_images_table.php
+++ multi_upload_blog/database/migrations/2019_08_11_061159_create_post_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreatePostImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('post_images', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned();
-            $table->string('title');
-            $table->string('body');
+            $table->integer('post_id')->unsigned();
+            $table->string('post_image_path');
+            $table->string('post_image_caption')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('post_images');
     }
 }
